a76e4dee6df96461a55803b5d7b728e2e257d0fe
deebu-ja <deebu.jacob@nrdtech.io>
2025-03-30 08:59:04 -0500

    initial  commit

---
+++ CSharpParallelThreadApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using CSharpParallelThreadApp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+
+        // Resolve the Worker service and call the Run method
+        var worker = host.Services.GetRequiredService<Worker>();
+        worker.Run().Wait();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+          .ConfigureAppConfiguration((hostingContext, config) =>
+          {
+              //var env = hostingContext.HostingEnvironment;
+              //Console.WriteLine($"Current Environment: {env.EnvironmentName}");
+              config.AddJsonFile("appSettings.json", optional: false, reloadOnChange: true);
+              config.AddJsonFile($"appSettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: false, reloadOnChange: true);
+          })
+        .ConfigureServices((hostContext, services) =>
+        {
+            // Register the Worker class as a service
+            services.AddTransient<Worker>();
+        });
+}
---
+++ CSharpParallelThreadApp/Worker.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Threading.Channels;
+using System.Collections.Concurrent;
+
+namespace CSharpParallelThreadApp
+{
+    public class Worker
+    {
+        private IConfiguration _configuration;
+        public Worker(IConfiguration configuration) { 
+            _configuration = configuration; 
+        }
+        public async Task Run()
+        {
+            Console.WriteLine($"Started running {_configuration.GetSection("HostData").Value}");
+
+            const int totalTasks = 10;
+            const int maxConcurrent = 2;
+
+            Console.WriteLine($"Starting {totalTasks} tasks with max {maxConcurrent} concurrent...");
+            // Create a range of numbers to process
+            var numbers = Enumerable.Range(1, totalTasks);
+
+            var options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = maxConcurrent
+            };
+
+            /*Channel*/
+            var channel = Channel.CreateBounded<int>(totalTasks);
+            await Parallel.ForEachAsync(numbers, options, async (number, ct) => {
+                var random = new Random();
+                Console.WriteLine($"Task {number} started");
+                await Task.Delay(500, ct);
+                await channel.Writer.WriteAsync(number * 2, ct);
+            });
+            channel.Writer.Complete();
+            await foreach (var result in channel.Reader.ReadAllAsync())
+            {
+                Console.WriteLine($"Task {result / 2}: Result: {result}");
+            }
+
+            /*Concurrent bag*/
+            ConcurrentBag<int> concurrentBag = new ConcurrentBag<int>();
+            await Parallel.ForEachAsync(numbers, options, async (number, ct) => {
+                var random = new Random();
+                Console.WriteLine($"Task {number} started");
+                await Task.Delay(100, ct);
+                concurrentBag.Add(number * 2);
+            });
+
+            foreach (int data in concurrentBag)
+            {
+                Console.WriteLine($"ConcurentBag-{data}");
+            }
+
+            Console.WriteLine("All tasks completed!");
+        }
+    }
+}
